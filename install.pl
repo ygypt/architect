b2fb84a186a8ce564130ac3ee2bf5cf6208773ce
ygypt <70084790+ygypt@users.noreply.github.com>
2023-10-05 00:03:48 -0500

    spent way too long adding stupid fucking visuals

--- install.pl
+++ install.pl
@@ -1,42 +1,90 @@
 #!/bin/perl
 use strict;
 use 5.010;
+use utf8;
+use open qw(:std :utf8);
 
-sub clr() {
-  my $cols = `tput cols`;
-  my $bar = "─" x $cols;
+sub clr {
+  my ($menu_title) = @_;
+
   system("clear");
-  say($bar);
-  say("Architect Installer by Ygypt");
-  say($bar);
-  system("lsblk");
-  say($bar);
+  
+  bar_top();
+  label("Architect Installer by Ygypt");
+  bar_mid();
+
+  foreach (split(/\n/, `lsblk`)) {
+    chomp($_);
+    label($_);
+  }
+  
+  bar_mid();
+  label("$menu_title");
+  bar_mid("╌");
 }
+
+sub bar {
+  my $symbol = "━";
+  if (@_) { $symbol = @_[0]; }
+  my $cols = `tput cols`;
+  my $bar = "$symbol" x ($cols - 2);
+  return $bar;
+}
+
+sub bar_flat  { say("━",bar(@_),"━"); }
+sub bar_top   { say("┏",bar(@_),"┓"); }
+sub bar_bot   { say("┗",bar(@_),"┛"); }
+sub bar_mid   { say("┣",bar(@_),"┫"); }
+
+sub label {
+  my ($text) = @_;
+  my $cols = `tput cols`;
+  my $text_len = length($text);
+  my $blank_len = $cols - $text_len - 3; # symbols on l/r and space on left
+  my $blank_bar = " " x $blank_len;
+  say("┃ $text$blank_bar┃");
+}
+sub label_full {
+  my ($text) = @_;
+  mkbar_top();
+  label($text);
+  mkbar_bot();
+}
+
 
 # main loop
 while(1){
-  clr();
-  say("Create a partition for '/'. Do not create home, it will be a btrfs subvolume");
-  say("Create a partition for '/efi' if it does not already exist");
-  say("");
-  say("What would you like to do?");
-  say("1: Edit disk \(cfdisk\)");
-  say("2: Format disk");
-  say("");
+  clr("Main Menu");
+  label("Create a partition for '/'. Do not create home, it will be a btrfs subvolume");
+  label("Create a partition for '/efi' if it does not already exist");
+  label("");
+  label("What would you like to do?");
+  label("1: Create disk partitions (cfdisk)");
+  label("2: Format disk (mkfs)");
+  bar_bot();
   print("Type number and press enter: ");
 
   chomp(my $menu = <>);
+  
   if ($menu == 1) {
-    cfdisk_menu();
+    partition_menu();
+    next;
+  }
+  
+  if ($menu == 2) {
+    format_menu();
+    next;
   }
 }
 
-sub cfdisk_menu() {
+
+sub partiiton_menu() {
   while(1) {
-    clr();
+    clr("Partition Menu");
     print("Type /dev/\'device name\' or type 'back': ");
     
     chomp(my $dev = <>);
+    
     if ($dev eq "back") {
       return;
     }
@@ -47,3 +95,35 @@ sub cfdisk_menu() {
     }
   }
 }
+
+
+sub format_menu() {
+  while(1) {
+    clr("Format Menu");
+    say("First we will format the ESP to FAT32");
+    say("");
+    say("Type 'skip' if you already have an ESP on your machine");
+    print("Otherwise, enter the /dev/device_name of your ESP: ");
+    
+    chomp(my $dev = <>);
+    
+    if ($dev eq "skip") {
+      return;
+    }
+
+    if (system("mkfs.fat -F 32 $dev")) {
+      print("Press enter to continue: ");
+      <>;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ test.pl
@@ -0,0 +1,5 @@
+#!/bin/perl
+use strict;
+use 5.010;
+
+say("fucko \\mcgeee");
